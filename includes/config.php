15966e5869864df8f7064e7b8d1b51461403dddd
sebastian muñoz <smunoz2025@alu.uct.cl>
2026-06-19 17:42:09 -0400

    update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,23 +5,26 @@
 // [PEDAGÓGICO] Los estudiantes aprenden a separar configuración
 // del código. Cambiando estas constantes pueden conectar a
 // cualquier servidor MySQL (localhost, producción, etc.)
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'ecommerce_uct');
 
 // Configuración de la aplicación
 define('SITE_NAME', 'Mi Tienda UCT');
-define('SITE_URL', 'http://localhost/ecommerce');
+define('SITE_URL', 'http://localhost/ecommerce-project');
 define('IVA', 19); // 19% IVA chileno
 define('COSTO_ENVIO', 4990); // $4.990 CLP
 define('RESERVA_MINUTOS', 10); // Las reservas expiran en 10 min
 
 // PayPal Sandbox
 define('PAYPAL_MODE', 'sandbox');
-define('PAYPAL_CLIENT_ID', '');
-define('PAYPAL_SECRET', '');
+define('PAYPAL_CLIENT_ID', 'TU_CLIENT_ID_SANDBOX');
+define('PAYPAL_SECRET', 'TU_SECRET_SANDBOX');
 define('PAYPAL_WEBHOOK_ID', '');
+
+// Variable global para scripts adicionales en el footer
+$scripts_adicionales = '';
 
 // Sesión
 ini_set('session.cookie_httponly', 1);
